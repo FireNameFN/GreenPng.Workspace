4810815c5f1c479d5c7cf56934e5b7024d12c1bb
FN <firenamefn@gmail.com>
2026-04-23 08:08:03 +0700

    Update to Git Workspace paradigm

--- GreenPng/ChunkType.cs
+++
@@ -1,9 +0,0 @@
-namespace GreenPng;
-
-public enum ChunkType {
-    IHDR = 0x49484452,
-    IDAT = 0x49444154,
-    IEND = 0x49454E44,
-    PLTE = 0x504C5445,
-    tRNS = 0x74524E53
-}
--- GreenPng/ImageType.cs
+++
@@ -1,9 +0,0 @@
-namespace GreenPng;
-
-public enum ImageType {
-    Greyscale = 0,
-    Truecolor = 2,
-    IndexedColor = 3,
-    GreyscaleAlpha = 4,
-    TruecolorAlpha = 6
-}
