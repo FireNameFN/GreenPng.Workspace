7e345e08c5aaf438122276a0a7aa92224caf3f06
FN <firenamefn@gmail.com>
2026-05-05 11:20:58 +0700

    Revert "Test fail"

--- GreenPng.Tests/DecodeTests.cs
+++ GreenPng.Tests/DecodeTests.cs
@@ -13,8 +13,6 @@ public sealed class DecodeTests {
 
         byte[] imageRef = Decoders.DecodeStbImageSharpBgra(testImage.Png);
 
-        Assert.Fail("Test fail");
-
         await Assert.That(image.SequenceEqual(imageRef)).IsTrue();
     }
 
